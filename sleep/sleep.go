2d7a4064ab6f2ec85f2446e4169466fd55c20c02
Michael Fellinger <m.fellinger@gmail.com>
2012-03-26 13:33:51 +0200

    fix and improve sleeping

--- sleep/sleep.go
+++ sleep/sleep.go
@@ -6,9 +6,12 @@ import (
 )
 
 func main() {
-	if len(os.Args) > 1 {
+	if len(os.Args) > 0 {
 		duration, err := time.ParseDuration(os.Args[1])
 		if err != nil {
+			duration, err = time.ParseDuration(os.Args[1] + "s")
+		}
+		if err == nil {
 			time.Sleep(duration)
 		}
 	}
