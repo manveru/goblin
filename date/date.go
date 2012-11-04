0e5d172de5f94c22e225504a225f70d2b2c52306
Michael Fellinger <m.fellinger@gmail.com>
2012-11-04 12:52:26 +0100

    date docs

--- date/date.go
+++ date/date.go
@@ -1,3 +1,26 @@
+// date and time
+//
+// SYNOPSIS
+//   date [ option ] [ seconds ]
+//
+// DESCRIPTION
+//   Print the date, in the format
+//
+//        Tue Aug 16 17:03:52 CDT 1977
+//
+//   The options are
+//
+//   -u   Report Greenwich Mean Time (GMT) rather than local
+//        time.
+//
+//   -n   Report the date as the number of seconds since the
+//        epoch, 00:00:00 GMT, January 1, 1970.
+//
+//   The conversion from Greenwich Mean Time to local time
+//   depends on the $timezone environment variable; see ctime(3).
+//
+//   If the optional argument seconds is present, it is used as
+//   the time to convert rather than the real time.
 package main
 
 import (
