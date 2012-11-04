d589d63dd9ce5e2f64cfa56c9e7fcc5742db8e5e
Michael Fellinger <m.fellinger@gmail.com>
2012-11-04 12:45:31 +0100

    cat docs.

--- cat/cat.go
+++ cat/cat.go
@@ -1,3 +1,37 @@
+// cat - catenate files
+//
+// SYNOPSIS
+//   cat [ file ... ]
+//   read [ -m ] [ -n nline ] [ file ... ]
+//   nobs [ file ... ]
+//
+// DESCRIPTION
+//   Cat reads each file in sequence and writes it on the stan-
+//   dard output.  Thus
+//
+//     cat file
+//
+//   prints a file and
+//
+//     cat file1 file2 >file3
+//
+//   concatenates the first two files and places the result on
+//   the third.
+//
+//   If no file is given, cat reads from the standard input.
+//   Output is buffered in blocks matching the input.
+//
+//   Read copies to standard output exactly one line from the
+//   named file, default standard input.  It is useful in inter-
+//   active rc(1) scripts.
+//
+//   The -m flag causes it to continue reading and writing multi-
+//   ple lines until end of file; -n causes it to read no more
+//   than nline lines.
+//
+// BUGS
+//   Beware of `cat a b >a' and `cat a b >b', which destroy input
+//   files before reading them.
 package main
 
 import (
